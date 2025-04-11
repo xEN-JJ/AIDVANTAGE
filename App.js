b5a8f7d7f29ca16fd5681ff54200f4850fdf025e
xEN-JJ <johnjosephasoro215@gmail.com>
2025-04-11 14:05:24 +0800

    Edit: New Branch

--- App.js
+++ App.js
@@ -6,6 +6,7 @@ export default function App() {
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <Text>Hello Worl</Text>
+      <Text>New Branch</Text>
       <StatusBar style="auto" />
     </View>
   );
